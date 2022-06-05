f8ed50fe4d4b684affbcad41c25eaea02cfad90c
DONG Xiangjing <dxj98@foxmail.com>
2022-06-05 23:51:13 +0200

    use models.py to create tables and migrate to mysql

--- README.md
+++ README.md
@@ -9,3 +9,29 @@ django-admin startproject [projectname]
 python manage.py startapp [appname]
 ```
 Register the app in settings.py
+
+## Connect to MySQL databases
+start mysql server
+```
+sudo mysqldsafe
+```
+
+Add databases config parameters in `setting.py`
+```
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'toydb',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
+    }
+}
+```
+Use the following 2 commands to let django create tables
+```
+python manage.py makemigrations  
+python manage.py migrate
+```
+or you can use `Tools`->`Run manage.py Task` in Pycharm
--- web/models.py
+++ web/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=32)
+
+
+class UserInfo(models.Model):
+    name = models.CharField(verbose_name='name', max_length=48)
+    password = models.CharField(verbose_name='password', max_length=64)
+    age = models.IntegerField(verbose_name='age')
+    account = models.DecimalField(verbose_name='credit', max_digits=10, decimal_places=2, default=0)
+    create_time = models.DateTimeField(verbose_name='enroll time')
+
+    # Foreign Key to Department id
+    # on_delete specifies what should be done when a row in Department is deleted
+    # there are many options: CASCADE, SET_NULL
+    department_id = models.ForeignKey(to='Department', to_field='id', on_delete=models.CASCADE)
+    # department_id = models.ForeignKey(to='Department', to_field='id', blank=True,
+    #                                   null=True, on_delete=models.SET_NULL)
+
+    gender_choices = (
+        (1, 'male'),
+        (2, 'female'),
+        (3, 'trans')
+    )
+    gender = models.SmallIntegerField(verbose_name='gender', choices=gender_choices)
+
+
